12d11cdef640fe8011680d66b111ee6e004f4885
Justin D. Harris <justin@pi>
2020-05-17 18:29:43 -0400

    Trying to send commands and pair Switch Doesn't work through this folder. With try with submodule.

--- server/switchremoteplay/server.py
+++ server/switchremoteplay/server.py
@@ -1,17 +1,24 @@
+import asyncio
+import logging
 import os
+import sys
 
-from flask import Flask, render_template
-from flask_socketio import SocketIO, send, emit
+from flask import Flask
+from flask_socketio import SocketIO
+#from flask_socketio import emit, send
+
+from controller import get_controller, push_button
 
 # Follow the example at https://flask-socketio.readthedocs.io/en/latest/
 
-print("Setting up")
+logger = logging.getLogger(__name__)
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') or 'asdasdasdf'
+# print(app.config['SECRET_KEY'])
 socketio = SocketIO(app, cors_allowed_origins='*')
 
-print(socketio)
-print(type(socketio))
+controller = None
 
 @socketio.on('connect')
 def test_connect():
@@ -24,19 +31,44 @@ def test_disconnect():
 # Handle unnamed events.
 @socketio.on('message')
 def handle_message(message):
-	print('received message: ' + message)
+    print('received message: ' + message)
 
 # Handle unnamed events with JSON.
 @socketio.on('json')
 def handle_json(json):
-	print('received json: ' + str(json))
+    print('received json: ' + str(json))
 
 @socketio.on('p')
-def handle_press(key):
-    print("will press: {}".format(key))
-    # TODO Send to joycontrol
-    return "PRESSED {}".format(key)    
+def handle_press(button):
+    print("will press: {}".format(button))
+    push_button(controller, button)
+    return "PRESSED {}".format(button)
+
+async def _main():
+    print("Starting")
+
+    bluetooth_address = sys.argv[1] if len(sys.argv) > 1 else None
+
+    print("Setting up with bluetooth address: {}".format(bluetooth_address))
+
+    global controller
+    controller = await get_controller(bluetooth_address)
+    print(controller)
+    import time
+    while True: time.sleep(1)
+
+
+    host = '0.0.0.0'
+    # 5000 is the default port.
+    port = 5000
+    #port = 48668
+    #print("Running at {}:{}".format(host, port))
+    #socketio.run(app, host, port)
+
 
 if __name__ == '__main__':
-    print("Starting")
-    socketio.run(app)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(
+        _main()
+    )
+    print("done")
