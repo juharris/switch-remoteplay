b24f5aec757f7f659aeef638a949e31be9a90f56
Justin Harris <justindharris@gmail.com>
2020-06-08 21:59:16 -0400

    Show Joysticks moving.

--- website-client/src/components/Controller/Joystick/Joystick.tsx
+++ website-client/src/components/Controller/Joystick/Joystick.tsx
@@ -2,16 +2,23 @@ import React from 'react'
 import classes from './Joystick.module.css'
 
 const Joystick: React.FunctionComponent<any> = (props: any) => {
-	let classList = classes.Joystick
-	const movedThreshold = 0.1
-	if (props.pressed || Math.abs(props.x) > movedThreshold || Math.abs(props.y) > movedThreshold) {
-		classList += " " + classes.Pressed
+	let joystickHolderClassList = classes.joystickHolder
+	let joystickClassList = classes.joystick
+	const movedThreshold = 0.15
+	if (props.pressed) {
+		joystickHolderClassList += " " + classes.pressed
+	}
+	if (Math.abs(props.x) > movedThreshold || Math.abs(props.y) > movedThreshold) {
+		joystickClassList += " " + classes.pressed
 	}
 
 	const styles = {
 		transform: `translate(${props.x * 15}px, ${props.y * 15}px)`,
 	}
-	return <div className={classList} style={styles}></div>
+	return <div className={joystickHolderClassList}>
+		<div className={joystickClassList} style={styles}>
+		</div>
+	</div>
 }
 
 export default Joystick
